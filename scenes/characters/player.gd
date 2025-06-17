6074e71e74bb189240a27c198e4ba7fc8476e9b0
Gonzalo <gonzalogallardo216@gmail.com>
2025-06-17 17:26:18 -0300

    episodio 2 terminado, agregue la animacion del tackle

--- scenes/characters/player.gd
+++ scenes/characters/player.gd
@@ -1,23 +1,39 @@
 class_name Player
 extends CharacterBody2D
 
+const DURATION_TACKLE := 200
+
 enum ControlScheme {CPU, P1, P2}
+enum State {MOVING, TACKLING}
 
 @export var control_scheme : ControlScheme
 @export var speed : float
 
 @onready var animation_player : AnimationPlayer = %AnimationPlayer
 @onready var player_sprite : Sprite2D = %PlayerSprite
+
 var heading:= Vector2.RIGHT
+var state := State.MOVING
+var time_start_tackle := Time.get_ticks_msec()
 
-
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if control_scheme == ControlScheme.CPU:
 		pass #process AI movement
 	else:
-		handle_human_movement()
+		if state == State.MOVING:
+			handle_human_movement()
+			if velocity.x != 0 and KeyTools.is_action_just_pressed(control_scheme, KeyTools.Action.SHOOT):
+				state = State.TACKLING
+				time_start_tackle= Time.get_ticks_msec()
+			set_movement_animation()
+		elif  state == State.TACKLING:
+			animation_player.play("tackle")
+			if Time.get_ticks_msec() - time_start_tackle > DURATION_TACKLE:
+				state = State.MOVING
+			
+		
 	
-	set_movement_animation()
+	
 	set_heading()
 	move_and_slide()
 	flip_sprites()
